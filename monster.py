c2119f6814fea74aecfa90b57aa411f029b2dca2
Carlo Mannino <mannicar@gmail.com>
2017-10-10 14:20:48 -0700

    Added exercises to monster.py

--- monster.py
+++ monster.py
@@ -6,7 +6,7 @@ class Monster(object):
         self.crimes = []
         self._isMisunderstood = True
         self.name = name
-        self._ssn = ssn
+        self._ssn = ssn # Because even Monsters pay taxes
         self.victims = set([])
 
     def roar(self):
@@ -23,12 +23,46 @@ class Monster(object):
         if self.name:
             print 'HELP! ' + self.name + ' murdered ' + victimName + '!'
 
+    # properties should be used to access variables
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, value):
+        self.name = value
+
+    # Implement victims setter and getter
+
+    # Implement SSN setter and getter
+
+    # =========== ADVANCED =========== 
+    # 1. Ensure that all items in crimes are strings
+    # 2. Add a date to all crimes.  HINT: Use a dictionary or a tuple.  You may have to modify the code you implemented in step one.
+
 class Vampire(Monster):
 
     def __init__(self, ssn, name, master):
+        # A subclass should initialize its parent class
         Monster.__init__(self, ssn, name)
         self.master = master
 
+    # This method overrides the murder method in the Monster class
     def murder(self, victimName):
         super(Vampire, self).murder(victimName)
         return Vampire(None, victimName, self.name)
+
+    def drink_blood(self, victimName):
+        return 'Not Implemented'
+        ## Implement me
+
+    def getTanned(self):
+        return 'Not Implemented'
+
+    # How do setters and getters work in here?
+    # If you did the VERY ADVANCED exercise, how will that change affect ssn and name setters and getters?   
+
+# =========== VERY ADVANCED =========== 
+class Person(object):
+
+    ## Implement me and make Vampire inherit from Person as well.  Remember people also have SSN!
